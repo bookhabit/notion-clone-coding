bffbab9e6f02468c4200f61179331dc3ab311aa0
bookhabit <qnrgoqlt@naver.com>
2023-10-26 15:39:09 +0900

    Authentication - convex와 clerk 연동

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import { Spinner } from "@/components/spinner";
 import { Button } from "@/components/ui/button";
+import { SignInButton } from "@clerk/clerk-react";
+import { useConvexAuth } from "convex/react";
 import { ArrowRight } from "lucide-react";
+import Link from "next/link";
 
 export const Heading = ()=>{
+    const {isAuthenticated,isLoading} = useConvexAuth()
+
     return(
         <div className="max-w-3xl space-y-4">
             <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">Your Ideas,Documents, & Plans. Unified. Welcome to <span className=" underline">Notion</span></h1>
@@ -11,10 +17,32 @@ export const Heading = ()=>{
                 Notion is the connected workspace where<br/>
                 better, faster work happens.
             </h3>
-            <Button>
-                Enter Notion
-                <ArrowRight className="h-4 w-4 ml-2"/>
-            </Button>
+            {/* 로그인 중 로딩상태 */}
+            {
+                isLoading && (
+                    <div className=" w-full flex items-center justify-center">
+                        <Spinner size={'lg'} />
+                    </div>
+                )
+            }
+            {/* 로그인 된 상태 */}
+            {isAuthenticated && !isLoading && (
+                <Button asChild>
+                    <Link href={'/documents'}>
+                        Enter Notion
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Link>
+                </Button>
+            )}
+            {/* 로그인 하지 않은 상태 */}
+            {!isAuthenticated && !isLoading && (
+                <SignInButton>
+                    <Button>
+                        Get Notion free
+                        <ArrowRight className="h-4 w-4 ml-2"/>
+                    </Button>
+                </SignInButton>
+            )}
         </div>
     )
 }
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://maximum-anemone-89.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  }
